b29078b1bdf5aaaaaf4d73e178796da6c7e19d67
Zhaorui <zhaorui@ualberta.ca>
2015-10-06 10:39:48 -0600

    partialy finished

---
+++ DVDCollector/app/src/androidTest/java/com/example/teppie/dvdcollector/Model/InternetTest.java
@@ -0,0 +1,27 @@
+package com.example.teppie.dvdcollector.Model;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by teppie on 06/10/15.
+ */
+public class InternetTest extends ActivityInstrumentationTestCase2 {
+
+    public void testIsConnected() throws Exception {
+
+    }
+
+    public void testPushMakeOnline() throws Exception {
+
+    }
+
+    public void testPushTradeOnline() throws Exception {
+
+    }
+
+    public void testCacheOffline() throws Exception {
+
+    }
+}
--- DVDCollector/app/src/main/java/com/example/teppie/dvdcollector/Model/Internet.java
+++ DVDCollector/app/src/main/java/com/example/teppie/dvdcollector/Model/Internet.java
@@ -4,5 +4,17 @@ package com.example.teppie.dvdcollector.Model;
  * Created by teppie on 05/10/15.
  */
 public class Internet {
-    public boolean
+    private boolean connectability = Boolean.FALSE;
+
+    public boolean isConnected(){
+        return this.connectability;
+    }
+
+    public void pushMakeOnline(){
+
+    }
+
+    public void pushTradeOnline(){}
+
+    public void cacheOffline(){}
 }
